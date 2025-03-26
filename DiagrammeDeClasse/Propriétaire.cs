2592d10b2be1f8b4e14d2502c9a8e214e250351d
Valerie <stpierre.v2016@gmail.com>
2025-03-26 14:30:28 -0400

    Update Propriétaire.cs

--- DiagrammeDeClasse/Propriétaire.cs
+++ DiagrammeDeClasse/Propriétaire.cs
@@ -1,11 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class Propriétaire : Utilisateur, IVendeur, IFournisseur
+public class Proprietaire : Utilisateur, IVendeur, IFournisseur
 {
+	public Proprietaire(string n, string p, string addr, string tel)
+	{
+		this.nom = n;
+		this.prenom = p;
+		this.adresse = addr;
+		this.telephone = tel;
+	}
 	public void AjouterUtilisateur()
 	{
 		throw new NotImplementedException();
